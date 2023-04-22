96b69e1e225877d9ae84d1601f90d55172e111d3
khalidzahra <khalidzahra923@gmail.com>
2023-04-23 01:40:20 +0200

    Fix path prefix error for api router

--- app/api/api.py
+++ app/api/api.py
@@ -3,7 +3,7 @@ from api.endpoints import user
 
 
 api_router = APIRouter()
-api_router.include_router(user.router, prefix="api/user", tags=["user"])
-api_router.include_router(user.router, prefix="api/ticket", tags=["ticket"])
-api_router.include_router(user.router, prefix="api/venue", tags=["venue"])
-api_router.include_router(user.router, prefix="api/contactperson", tags=["contactperson"])
+api_router.include_router(user.router, prefix="/api/user", tags=["user"])
+api_router.include_router(user.router, prefix="/api/ticket", tags=["ticket"])
+api_router.include_router(user.router, prefix="/api/venue", tags=["venue"])
+api_router.include_router(user.router, prefix="/api/contactperson", tags=["contactperson"])
