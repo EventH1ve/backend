c00e02581779d18c814982a30f649087e9066c20
khalidzahra <khalidzahra923@gmail.com>
2023-04-23 01:22:17 +0200

    Add contact person endpoint

---
+++ app/api/endpoints/contact_person.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from fastapi_sqlalchemy import db
+from schemas.venue import ContactPerson
+from models.venue import ContactPerson as ModelContactPerson
+
+
+router = APIRouter()
+
+
+@router.post('/contact')
+async def registerContactPerson(contactPerson: ContactPerson):
+    contactModel = ModelContactPerson(**contactPerson.dict())
+
+    db.session.add(contactModel)
+    db.session.commit()
+
+    return {
+        "success": True,
+        "message": "Contact person registered."
+    }
