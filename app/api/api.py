7823c966f5ede425590e6808f80b5280b769e22b
khalidzahra <khalidzahra923@gmail.com>
2023-04-24 00:15:22 +0200

    Fix improper endpoint imports for routers

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from api.endpoints import user
+from api.endpoints import user, ticket, venue, contact_person as contactPerson
 
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/api/user", tags=["user"])
-api_router.include_router(user.router, prefix="/api/ticket", tags=["ticket"])
-api_router.include_router(user.router, prefix="/api/venue", tags=["venue"])
-api_router.include_router(user.router, prefix="/api/contactperson", tags=["contactperson"])
+api_router.include_router(ticket.router, prefix="/api/ticket", tags=["ticket"])
+api_router.include_router(venue.router, prefix="/api/venue", tags=["venue"])
+api_router.include_router(contactPerson.router, prefix="/api/contactperson", tags=["contactperson"])
