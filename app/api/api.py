0c2f3c90aca61e0789cee6c6a5f38496b39dcd49
khalidzahra <khalidzahra923@gmail.com>
2023-04-27 06:01:51 +0200

    Add ticket type endpoints

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from api.endpoints import user, ticket, venue, contact_person as contactPerson
-
+from api.endpoints import event
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/api/user", tags=["user"])
 api_router.include_router(ticket.router, prefix="/api/ticket", tags=["ticket"])
 api_router.include_router(venue.router, prefix="/api/venue", tags=["venue"])
 api_router.include_router(contactPerson.router, prefix="/api/contactperson", tags=["contactperson"])
+api_router.include_router(event.router, prefix="/api/event", tags=["event"])
--- app/api/endpoints/ticket.py
+++ app/api/endpoints/ticket.py
@@ -1,6 +1,27 @@
+from typing import List
 from fastapi import APIRouter
 from fastapi_sqlalchemy import db
-from schemas.ticket import Ticket
+from schemas.ticket import Ticket, TicketType
+from models.ticket import Ticket as ModelTicket, TicketType as ModelTicketType
 
 
 router = APIRouter()
+
+
+@router.post('/type')
+async def createTicketType(ticketType: TicketType):
+    ticketTypeModel = ModelTicketType(**ticketType.dict())
+
+    db.session.add(ticketTypeModel)
+    db.session.commit()
+
+    return {
+        "success": True,
+        "message": "Ticket type created."
+    }
+
+
+@router.get('/type', response_model=List[TicketType])
+async def listTicketTypes():
+    ticketTypes = db.session.query(ModelTicketType).all()
+    return ticketTypes
