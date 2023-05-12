d9deb31c241f7553a865b3e7009793fac4989959
khalidzahra <khalidzahra923@gmail.com>
2023-05-12 18:52:32 +0300

    Add .env.example and add logging middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi_sqlalchemy import DBSessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from api.api import api_router
@@ -14,7 +14,14 @@ app = FastAPI()
 
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
-origins = ["*"]
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    print(request.method, request.url)
+    response = await call_next(request)
+    return response
+
+
+origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
