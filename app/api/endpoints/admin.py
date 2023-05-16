0b51c7d9f2feca183b7c74bd3723c33a2143db3c
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-05-16 16:52:14 +0200

    added retrieval of specific event and all events of admin

--- app/api/endpoints/admin.py
+++ app/api/endpoints/admin.py
@@ -41,3 +41,41 @@ async def createEvent(event: ModelAdminEvent, userId: Annotated[int, Depends(get
         "message": "Event created."
     }
 
+@router.get('/event', response_model=List[ListEvent])
+async def getAdminEvents(userId: Annotated[int, Depends(getCurrentUserId)], skip: int = 0, limit: int = 10):
+    events = db.session.query(ModelEvent).with_entities(
+        ModelEvent.id,
+        ModelEvent.name,
+        ModelEvent.venue,
+        ModelEvent.datetime.label("date"),
+        ModelEvent.profile,
+    ).filter(ModelEvent.adminid == userId).group_by(ModelEvent.id).offset(skip).limit(limit).all()
+        
+    return events
+
+@router.get('/event/{id}', response_model=SingleEvent)
+async def getAdminEvent(id: int, userId: Annotated[int, Depends(getCurrentUserId)]):
+    event = db.session.query(ModelEvent).with_entities(
+        ModelEvent.id,
+        ModelEvent.name,
+        ModelEvent.venue,
+        ModelEvent.description,
+        ModelEvent.datetime.label("date"),
+        ModelEvent.profile,
+    ).filter(ModelEvent.adminid == userId, ModelEvent.id == id).first()     
+
+    ticketTypes = db.session.query(ModelTicketType).filter(ModelTicketType.eventid == event.id).all()
+    eventBookingCount = len( db.session.query(UserEventBooking).filter(UserEventBooking.eventid == event.id).all() )
+
+    eventDict = {
+        "name": event.name,
+        "venue": event.venue,
+        "date": event.date,
+        "description": event.description,
+        "cover": event.profile,
+        "attendess": eventBookingCount,
+        "tickettypes": ticketTypes
+    }
+        
+    return eventDict
+
